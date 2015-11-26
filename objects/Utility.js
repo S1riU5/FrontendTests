f423129f3a00d0ef99601098879522d140ded9fe
S1riU5 <s0537090@htw-berlin.de>
2015-11-27 00:06:58 +0100

    Added package.json and protractor as dependency

--- objects/Utility.js
+++ objects/Utility.js
@@ -3,7 +3,7 @@ var Login = require('./Login.js');
 var Utility = function(){
 
     this.login = function(){
-        Login.loginNoInput();
+        Login.login('local@ceventis.com','password');
         expect(browser.getLocationAbsUrl()).toBe('/devices');
     };
 
